a65c6b00c2937265adca5bffa43ed4036b45444b
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-31 01:42:47 +0700

    Add some comments for detail explanation

--- Simple-Reddit-Clone/src/app/article/article.component.ts
+++ Simple-Reddit-Clone/src/app/article/article.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit, HostBinding } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+
+  // A component host is the element this component is attached to (component selector tag)
+  // Anotation HostBinding is used to set property to the host element
+  // Pros of using host element
+  // - Don't both use app-article tag and require a class="row" in the markup of the parent element view
+  //   E.g.: instead of using <div class="article-wrapper row"><app-article></app-article></div>
+  //         we use <app-article class="row"></app-article>
+  // - Be able to configure our host element within the component
+
   @HostBinding('attr.class') cssClass = 'row';
   point: number;
   title: string;
@@ -22,6 +31,10 @@ export class ArticleComponent implements OnInit {
 
   voteUp(): boolean {
     this.point++;
+
+    // For the first time this method does not return anything. But this would cause the page reload whenever user clicks on the link
+    // This is the click event is propagated to parents and our browser will try to follow an empty link and reload the page
+
     return false;
   }
 

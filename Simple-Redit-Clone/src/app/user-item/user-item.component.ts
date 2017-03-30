ec5e576c962da1ed7a32c72bb0f68c7e45395bd7
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-30 13:25:12 +0700

    Make UserList component render list of UserItem component. Declare input in UserItem component. Pass value in UserItem component's input

--- Simple-Redit-Clone/src/app/user-item/user-item.component.ts
+++ Simple-Redit-Clone/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name: string; // Declare a new property `name` as being of string type
+  // Adding anotation @Input() before a property of a component make this property as an input of the component
+  // Two things included:
+  // - As it's an input: it can be passed in the component
+  // - As it's a property: it can be used within the component's template
+  @Input() name: string; // Declare a new property `name` as being of string type
 
   constructor() {
     this.name = 'NMT';

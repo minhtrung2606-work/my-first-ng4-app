b3d02be583de784ccfae6eb7833b41008fe5024a
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-30 12:46:55 +0700

    Init Simple-Redit-Clone app

---
+++ Simple-Redit-Clone/src/app/simple-redit-clone/simple-redit-clone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-redit-clone',
+  templateUrl: './simple-redit-clone.component.html',
+  styleUrls: ['./simple-redit-clone.component.css']
+})
+export class SimpleReditCloneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

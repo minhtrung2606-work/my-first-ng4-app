e0edbd4e6402a3a883f8ffad7a411d4ec738ac95
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-03 13:18:07 +0700

    Update Inventory app root component's seletor. Update Inventory component's template. Add some comments for detail explanation

--- Inventory/src/app/app.component.ts
+++ Inventory/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// Below is called Component Decorator which is used to configure how the outside world would use/interact your component
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'inventory-app', // Indicate how your component is recognized when used in a template
+  templateUrl: './app.component.html', // So called visual part of the component which is to declare HTML code for your component
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 }
--- Inventory/src/app/product.ts
+++ Inventory/src/app/product.ts
@@ -3,6 +3,6 @@ export class Product {
     public sku: string,
     public name: string,
     public imageUrl: string,
-    public department: string,
+    public departments: Array<string>,
     public price: number) {}
 }

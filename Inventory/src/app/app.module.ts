f00e1b1ade32a34636f4626cabc08995a89a3715
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-10 16:28:31 +0700

    Add some comments for detail explanation on How Angular bootstraps an application

--- Inventory/src/app/app.module.ts
+++ Inventory/src/app/app.module.ts
@@ -10,6 +10,18 @@ import { ProductImageComponent } from './components/product-image/product-image.
 import { ProductPriceComponent } from './components/product-price/product-price.component';
 import { ProductDepartmentsComponent } from './components/product-departments/product-departments.component';
 
+// At first, module system (introduced by Angular) helps organize your code
+// Compared to AngularJS 1.x, you have to specify which components you're gonna be using in your app
+
+// While it's a bit more configuration at this time, but it's truly a lifesave for larger apps
+
+// When a new component is created, it MUST be accessible in the current module to be used which says
+// If ProductListComponent is required to use with product-list selector in the AppComponent template then we need to
+// make sure that the AppComponent's module either:
+// - is in the same module as the ProductListComponent or
+// - the AppComponent module has to import the module containing ProductListComponent
+// (They're in the same module in this exmaple app)
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +37,8 @@ import { ProductDepartmentsComponent } from './components/product-departments/pr
     HttpModule
   ],
   providers: [],
+
+  // AppComponent is the top-level component which will be bootstrapped
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Inventory/src/main.ts
+++ Inventory/src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// This file will be treated as the entry point of this application which is declared in .angular-cli.json file
+// under apps.main path
+
+// Here's the whole flow to serve a server using `ng serve` command:
+// 1. It looks at .angular-cli.json file to find the declared entry point under apps.main and apps.index paths
+// 2. The entry point TypeScript file will boots up the top-level component
+// 3. Top-level component renders the rest of our app
+
 if (environment.production) {
   enableProdMode();
 }

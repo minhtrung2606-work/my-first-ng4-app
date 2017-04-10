afb0c07059aac1b524320341807e43b18dd484dc
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-10 13:26:33 +0700

    Add some comments for detail explanation

--- Inventory/src/app/components/product-list/product-list.component.ts
+++ Inventory/src/app/components/product-list/product-list.component.ts
@@ -7,9 +7,14 @@ import { Product } from './../../product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  // An array of type Product passed in this component
   @Input() productList: Array<Product>;
+
+  // Pass out the current product whenever a product is selected
+  // Selected is done by clicked and marked selected
   @Output() onProductSelected: EventEmitter<Product>;
 
+  // Private property of this component to keep the current selected product
   private currentProduct: Product;
 
   constructor() {
@@ -25,6 +30,7 @@ export class ProductListComponent implements OnInit {
   }
 
   isProductSelected(product: Product): boolean {
+    // Use equals function of the type Product to make it easy to test
     return product && this.currentProduct && this.currentProduct.equals(product);
   }
 

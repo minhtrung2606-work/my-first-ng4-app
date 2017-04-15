fe299bc3c4a6c6fe207695e28d67f1645e51786f
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-15 22:22:23 +0700

    Add some comments for detail explanation

--- Inventory/src/app/components/product-form/product-form.component.ts
+++ Inventory/src/app/components/product-form/product-form.component.ts
@@ -10,6 +10,12 @@ export class ProductFormComponent implements OnInit {
   myForm: FormGroup;
 
   constructor(fb: FormBuilder) {
+
+    // Create a new instance of FormGroup by calling group method of FormBuilder service
+    // A property of an object passed in the method group contains a value of an array will add a new instance of FormControl
+    // Newly added FormControl will be used in the template like this
+    // <input [formControl]="myForm.controls['sku']">
+
     this.myForm = fb.group({
       sku: ['sample-sku']
     });

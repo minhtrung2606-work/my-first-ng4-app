d5bb534fbcd14b6f6d42635061b56378449bca78
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-24 20:23:29 +0700

    Update CompleteProductForm component inner validator class

--- Inventory/src/app/components/complete-product-form/complete-product-form.component.ts
+++ Inventory/src/app/components/complete-product-form/complete-product-form.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 class InAppValidator {
-  static buildStartWithValidator(str: string): ValidatorFn {
-    return function (control: AbstractControl) {
+  static buildStartWithValidator(validatorName: string, str: string): ValidatorFn {
+    return function (control: AbstractControl): {[s: string]: boolean} {
       let regEpx = new RegExp('^' + str, 'i');
       let valid = regEpx.test(control.value);
-      console.log(valid);
-      return {
-        'start_with_sku_': valid
-      };
+      let result = {};
+      result[validatorName] = valid;
+      return result;
     };
   }
 }
@@ -24,7 +23,7 @@ export class CompleteProductFormComponent implements OnInit {
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      sku: ['sku_', Validators.compose([Validators.required, InAppValidator.buildStartWithValidator('sku_')])],
+      sku: ['sku_', Validators.compose([Validators.required, InAppValidator.buildStartWithValidator('start_with_sku', 'sku_')])],
       name: ['Unknown', Validators.required],
       departments: ['', Validators.required],
       price: [0, Validators.required]

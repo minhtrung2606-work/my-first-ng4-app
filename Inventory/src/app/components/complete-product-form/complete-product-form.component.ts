f2b90d409db8a386712b3e8a041d77b3e718d7df
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-04-25 20:39:00 +0700

    Update CompleteProductForm component to have review form by using EventEmitter of FormControl

--- Inventory/src/app/components/complete-product-form/complete-product-form.component.ts
+++ Inventory/src/app/components/complete-product-form/complete-product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { Product } from './../../product';
 
 class InAppValidator {
   static buildStartWithValidator(validatorName: string, str: string): ValidatorFn {
@@ -27,17 +28,52 @@ class InAppValidator {
 })
 export class CompleteProductFormComponent implements OnInit {
   private myForm: FormGroup;
+  private product: Product;
 
   constructor(fb: FormBuilder) {
+    this.product = new Product(
+      'sku_',
+      'Unknown',
+      'assets/resources/images/products/box.png',
+      ['Unkonwn'],
+      0.00
+    );
+
     this.myForm = fb.group({
-      sku: ['sku_', Validators.compose([Validators.required, InAppValidator.buildStartWithValidator('start_with_sku', 'sku_')])],
-      name: ['Unknown', Validators.required],
-      departments: ['Unknown', Validators.required],
-      price: [0, Validators.required]
+      sku: [this.product.sku, Validators.compose([Validators.required, InAppValidator.buildStartWithValidator('start_with_sku', 'sku_')])],
+      name: [this.product.name, Validators.required],
+      departments: [this.product.departments.join(''), Validators.required],
+      price: [this.product.price, Validators.required]
     });
   }
 
   ngOnInit() {
+
+    this.myForm.get('sku').valueChanges.subscribe(
+      (value) => {
+        this.product.setSku(value);
+      }
+    );
+
+    this.myForm.get('name').valueChanges.subscribe(
+      (value) => {
+        this.product.setName(value);
+      }
+    );
+
+    this.myForm.get('departments').valueChanges.subscribe(
+      (value) => {
+        let departments = (value || '').split(',');
+        this.product.setDepartments(departments);
+      }
+    );
+
+    this.myForm.get('price').valueChanges.subscribe(
+      (value) => {
+        this.product.setPrice(value);
+      }
+    );
+
   }
 
   onSubmit(product): void {
--- Inventory/src/app/product.ts
+++ Inventory/src/app/product.ts
@@ -6,6 +6,31 @@ export class Product {
     public departments: Array<string>,
     public price: number) {}
 
+  setSku(sku: string): Product {
+    this.sku = sku;
+    return this;
+  }
+
+  setName(name: string): Product {
+    this.name = name;
+    return this;
+  }
+
+  setImageUrl(imageUrl: string): Product {
+    this.imageUrl = imageUrl;
+    return this;
+  }
+
+  setDepartments(departments: Array<string>): Product {
+    this.departments = departments;
+    return this;
+  }
+
+  setPrice(price: number): Product {
+    this.price = price;
+    return this;
+  }
+
   equals(product: Product): boolean {
     return product && this.sku === product.sku;
   }
